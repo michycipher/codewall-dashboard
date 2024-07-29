b6b0b4b8c1fdc77520173ac72e2d297c94f7bcc1
michycipher <michelleutomi@yahoo.com>
2024-07-29 20:51:18 +0100

    initial commit

---
+++ app/page.tsx
@@ -0,0 +1,13 @@
+
+export default function Home() {
+  return (
+    <>
+    <h1>Dashboard</h1>
+    <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Suscipit a ducimus
+       delectus vel nobis excepturi adipisci sunt quod quibusdam beatae modi impedit 
+       soluta, expedita cumque est consectetur vitae debitis quos!
+    </p>
+    </>
+  );
+}
+
---
+++ components/NavBar.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const NavBar = () => {
+  return (
+    <div className='bg-primary dark:bg-slate-700 text-white py-2 px-5 flex justify-between'>
+      Navbar
+    </div>
+  )
+}
+
+export default NavBar
